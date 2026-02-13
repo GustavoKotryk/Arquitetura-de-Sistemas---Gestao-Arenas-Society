9489fe0340cd18d7f3ef3aac582c0fecf17b5482
matheus engel <matheusengelfl@gmail.com>
2026-02-13 15:45:49 -0300

    feat: estrutura do projeto

---
+++ src/main/java/org/example/infra/repository/MemoryReservaRepository.java
@@ -0,0 +1,4 @@
+package org.example.infra.repository;
+
+public class MemoryReservaRepository {
+}
---
+++ src/main/java/org/example/infra/strategies/PrecoHorarioPicoStrategy.java
@@ -0,0 +1,4 @@
+package org.example.infra.strategies;
+
+public class PrecoHorarioPicoStrategy {
+}
