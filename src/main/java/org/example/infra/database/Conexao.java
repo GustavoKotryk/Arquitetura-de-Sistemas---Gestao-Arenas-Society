19167a15c3741d9468dff3d5090e376fc1d55c5f
matheus engel <matheusengelfl@gmail.com>
2026-02-17 21:42:04 -0300

    removendo credenciais fake

--- src/main/java/org/example/infra/database/Conexao.java
+++ src/main/java/org/example/infra/database/Conexao.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    public static final String URL = "jdbc:mysql://gestao-arena-society-kotryk.l.aivencloud.com:19626/defaultdb?useSSL=true&trustServerCertificate=true&allowPublicKeyRetrieval=true";
-    public static final String USER = "avnadmin";
-    public static final String PASSWORD = "AVNS_K9v0DXJ_yDfMJpKUS0f";
+    public static final String URL = "teste";
+    public static final String USER = "teste";
+    public static final String PASSWORD = "teste";
 
     public static Connection conectar()throws SQLException{
         return DriverManager.getConnection(URL, USER, PASSWORD);
